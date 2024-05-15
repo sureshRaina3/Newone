da4f1580567a61f946597b5724bbeaa6cdbf1cdf
suresh <sureshpethurajan111@gmail.com>
2024-05-15 17:09:11 +0530

    Added monochrome

--- src/test/java/AmazonRunnerFile/JunitAmazonRunner.java
+++ src/test/java/AmazonRunnerFile/JunitAmazonRunner.java
@@ -3,11 +3,13 @@ package AmazonRunnerFile;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/java/Features",
-		glue= "StepDefenitions",stepNotifications=true
+		glue= "StepDefenitions",stepNotifications=true,
+		monochrome=true
 		)
 
 
